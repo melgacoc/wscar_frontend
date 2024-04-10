6d824f250e52882571229da95d708654d5c26f41
Cláudio Melgaço <claudio19a@hotmail.com>
2024-04-10 08:26:02 -0300

    Revertendo rota

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CarList from "./pages/carList.js";
 import NewCar from "./pages/newCar.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route exact path="/" element={<CarList />} />
         <Route exact path="/newCar" element={<NewCar />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
